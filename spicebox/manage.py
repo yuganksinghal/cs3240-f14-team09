7f695de199474850d101f9b8f886ed0e01f10544
Matt <Student@172.27.44.220>
2014-11-09 14:14:56 -0500

    added the Models to manage.py, did not run any command line initialization

--- spicebox/manage.py
+++ spicebox/manage.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.db import models
 
+class User(models.Model):
+    type = models.CharField(max_length= 50)
+    username = models.CharField(max_length= 50)
+class File(models.Model):
+    name = models.CharField(max_length= 50)
+    location = models.CharField(max_length= 200)
+
+class Bulletin(models.Model):
+    description = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    location = models.CharField(max_length=200)
+    author = models.ForeignKey(User)
+    file = models.ForeignKey(File)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spicebox.settings")
 
